6f9f9f9e97bbbff13b082f852646f163d86608de
jiayun <songbowen@xiaohongshu.com>
2023-08-24 20:27:33 +0800

    add: for loop to use kubectl command

--- shell/for_test.sh
+++ shell/for_test.sh
@@ -12,10 +12,10 @@ done
 
 # 根据node_name查node_ip，用于很多node，一次无法全部查出来
 result_str=""
-node_arr="k8s-sh-redis010144077017 k8s-sh-redis010144076206"
+node_arr="k8s-sh-redis010144064209 k8s-sh-redis010144067088 k8s-sh-redis010144066181 k8s-sh-redis010144079135 k8s-sh-redis010144079068 k8s-sh-redis010144072163 k8s-sh-redis010144066085 k8s-sh-redis010144065116 k8s-sh-redis010144079149 k8s-sh-redis010144070158"
 for i in $node_arr;
 do
-  ip=`kuberedisalish get node k8s-sh-redis010144077017 -owide | tail -n +2| awk '{print $6}'`
+  ip=`kuberedisalish get node $i -owide | tail -n +2| awk '{print $6}'`
   result_str="${result_str} ${ip}"
 done
 echo "${result_str}"
