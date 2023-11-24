4165d680f1be1dd4573cc8621cd6e3403cca67b5
jiayun <songbowen@xiaohongshu.com>
2023-11-24 17:56:56 +0800

    add: strA contains strB

--- shell/redis1.sh
+++ shell/redis1.sh
@@ -6,7 +6,7 @@ shopt -s expand_aliases
 pod_ips=$(kubectl get pod -l label_cluster=k8redis-search-ads-sug-alish-1 -owide --no-headers | awk '{print $6}')
 for ip in $pod_ips; do
   result=$(redis-cli -h "$ip" -p 6379 role)
-  if [[ result =~ 'master' ]]; then
+  if [[ $result =~ 'master' ]]; then
     echo "$result"
   fi
 done
